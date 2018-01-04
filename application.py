c078a3e87509b919ec72a7e90c1a3f0108603f2a
Kim Hung LE <kim.hung@greencityzen.fr>
2018-01-04 18:49:17 +0100

    Remove any bluemix relevant information

--- application.py
+++ application.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-import atexit
-
 import os
-import json
 import main
 
 
@@ -19,7 +16,6 @@ db = None
 
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
-port = int(os.getenv('PORT', 8000))
 
 @app.route('/')
 def home():
@@ -31,46 +27,5 @@ def run():
     detect_final_result = main.main_function(deviceID)
     return str(detect_final_result)
 
-# /* Endpoint to greet and add a new visitor to database.
-# * Send a POST request to localhost:8000/api/visitors with body
-# * {
-# *     "name": "Bob"
-# * }
-# */
-@app.route('/api/visitors', methods=['GET'])
-def get_visitor():
-    if client:
-        return jsonify(list(map(lambda doc: doc['name'], db)))
-    else:
-        print('No database')
-        return jsonify([])
-
-# /**
-#  * Endpoint to get a JSON array of all the visitors in the database
-#  * REST API example:
-#  * <code>
-#  * GET http://localhost:8000/api/visitors
-#  * </code>
-#  *
-#  * Response:
-#  * [ "Bob", "Jane" ]
-#  * @return An array of all the visitor names
-#  */
-@app.route('/api/visitors', methods=['POST'])
-def put_visitor():
-    user = request.json['name']
-    if client:
-        data = {'name':user}
-        db.create_document(data)
-        return 'Hello %s! I added you to the database.' % user
-    else:
-        print('No database')
-        return 'Hello %s!' % user
-
-@atexit.register
-def shutdown():
-    if client:
-        client.disconnect()
-
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=port, debug=True)
+    application.run(host='0.0.0.0')
